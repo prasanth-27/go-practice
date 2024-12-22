93dc1cc86b58726a21cc2dd669c30887eb8f1fcc
prasanth_27 <yvsaiprasanth@gmail.com>
2024-12-22 20:23:57 +0530

    Add First Module

--- exercises/ex_8/main.go
+++ exercises/ex_8/main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"fmt"
 
-	"github.com/prasanth-27/go-practice/exercises/ex_7/myPackage"
+	"github.com/prasanth-27/go-practice/modularExercises/first"
 )
 
 func main() {
 	fmt.Println("Using package")
 
-	myPackage.PackageFunc()
+	first.Firstfunc()
 }
--- exercises/go.mod
+++ exercises/go.mod
@@ -1,3 +1,5 @@
 module github.com/prasanth-27/go-practice/exercises
 
 go 1.22.5
+
+require github.com/prasanth-27/go-practice/modularExercises/first v0.0.0-20241222143819-b8b3393770db
